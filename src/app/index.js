c08e111471665060bdbd9567642946f6c38889dd
iampeterbanjo <peterbanjo@gmail.com>
2015-03-11 21:48:31 +0000

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('meme', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial', 'meme-background', 'meme-image', 'nav', 'meme-banner', 'about'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutCtrl'
+      });
+
+		$stateProvider
+      .state('meme', {
+        url: '/meme',
+        templateUrl: 'app/meme/meme.html',
+        controller: 'MemeCtrl'
+      });
+
+	$stateProvider
+      .state('meme.background', {
+        url: '/background',
+        templateUrl: 'app/meme-background/meme-background.html',
+        controller: 'MemeBackgroundCtrl'
+      });
+
+	$stateProvider
+      .state('meme.image', {
+        url: '/image',
+        templateUrl: 'app/meme-image/meme-image.html',
+        controller: 'MemeImageCtrl'
+      });
+
+	$stateProvider
+      .state('meme.banner', {
+        url: '/banner',
+        templateUrl: 'app/meme-banner/meme-banner.html',
+        controller: 'MemeBannerCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/meme/background');
+  })
+;
---
+++ src/app/meme-image/meme-image.controller.spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('controllers', function(){
+  var scope;
+
+  beforeEach(module('meme-image'));
+
+  beforeEach(inject(function($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should define more than 5 awesome things', inject(function($controller) {
+    expect(scope.awesomeThings).toBeUndefined();
+
+    $controller('MemeImageCtrl', {
+      $scope: scope
+    });
+
+    expect(angular.isArray(scope.awesomeThings)).toBeTruthy();
+    expect(scope.awesomeThings.length > 5).toBeTruthy();
+  }));
+});
