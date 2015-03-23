06cc473e482398d55408b5c1c863f322f5f28305
iampeterbanjo <peterbanjo@gmail.com>
2015-03-23 12:09:54 +0000

    update material theme

--- src/app/index.js
+++ src/app/index.js
@@ -1,42 +1,46 @@
 'use strict';
 
 angular.module('meme', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial', 'meme-background', 'meme-image', 'nav', 'meme-banner', 'about'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('about', {
-        url: '/about',
-        templateUrl: 'app/about/about.html',
-        controller: 'AboutCtrl'
-      });
+	.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+		$stateProvider
+			.state('about', {
+				url: '/about',
+				templateUrl: 'app/about/about.html',
+				controller: 'AboutCtrl'
+			});
 
 		$stateProvider
-      .state('meme', {
-        url: '/meme',
-        templateUrl: 'app/meme/meme.html',
-        controller: 'MemeCtrl'
-      });
+			.state('meme', {
+				url: '/meme',
+				templateUrl: 'app/meme/meme.html',
+				controller: 'MemeCtrl'
+			});
 
-	$stateProvider
-      .state('meme.background', {
-        url: '/background',
-        templateUrl: 'app/meme-background/meme-background.html',
-        controller: 'MemeBackgroundCtrl'
-      });
+		$stateProvider
+			.state('meme.background', {
+				url: '/background',
+				templateUrl: 'app/meme-background/meme-background.html',
+				controller: 'MemeBackgroundCtrl'
+			});
 
-	$stateProvider
-      .state('meme.image', {
-        url: '/image',
-        templateUrl: 'app/meme-image/meme-image.html',
-        controller: 'MemeImageCtrl'
-      });
+		$stateProvider
+			.state('meme.image', {
+				url: '/image',
+				templateUrl: 'app/meme-image/meme-image.html',
+				controller: 'MemeImageCtrl'
+			});
 
-	$stateProvider
-      .state('meme.banner', {
-        url: '/banner',
-        templateUrl: 'app/meme-banner/meme-banner.html',
-        controller: 'MemeBannerCtrl'
-      });
+		$stateProvider
+			.state('meme.banner', {
+				url: '/banner',
+				templateUrl: 'app/meme-banner/meme-banner.html',
+				controller: 'MemeBannerCtrl'
+			});
 
-    $urlRouterProvider.otherwise('/meme/background');
-  })
-;
+		$urlRouterProvider.otherwise('/meme/background');
+
+		// theming
+		$mdThemingProvider.theme('default')
+			.primaryPalette('pink')
+			.accentPalette('orange');
+	});
